714fe818be7b7a027bd8f0c2a884a8900d9cb27a
JinWon Oh <dhwlsdnjs55@naver.com>
2022-10-05 14:07:11 +0900

    spring_web

---
+++ out/artifacts/spring_web01_war_exploded/views/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dhwls
+  Date: 2022-10-05
+  Time: 오전 11:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>    <title>Title</title>
+</head>
+<body>
+    결과내용 : <b>${message}</b>
+<%--    <h1>asdad${message}</h1>--%>
+    <h1>웹/views 경로 hello</h1>
+</body>
+</html>
---
+++ src/main/java/kosta/controller/HelloController.java
@@ -0,0 +1,24 @@
+package kosta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+//    @RequestMapping("/hello.do")
+//    public ModelAndView hello() {
+//        ModelAndView mav = new ModelAndView();
+//        mav.addObject("message", "Hello Spring");
+//        mav.setViewName("hello");
+//        return mav;
+//    }
+
+    @RequestMapping("/hello2.do")
+    public String hello2(Model model) {
+        model.addAttribute("message", "Hellasdasdo Springasdasd");
+        return "hello";
+    }
+}
